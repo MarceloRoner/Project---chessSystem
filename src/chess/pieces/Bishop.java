95f0225b2d5ebd3399364d3249fe347a3cb4ec0c
Roner <marceloroner90@gmail.com>
2024-04-12 10:49:29 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,75 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColums()];
+
+		Position p = new Position(0, 0);
+
+		// nw
+
+		p.setValues(position.getRow() - 1, position.getColum() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setValues(p.getRow() - 1, p.getColum() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+
+		}
+
+		// ne
+
+		p.setValues(position.getRow() - 1, position.getColum() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setValues(p.getRow() - 1, p.getColum() -1 );
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+
+		}
+
+		// right
+		p.setValues(position.getRow(), position.getColum() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setColum(p.getColum() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+
+		}
+
+		// bellow
+		p.setValues(position.getRow() + 1, position.getColum());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+
+		}
+
+		return mat;
+	}
+
+}
